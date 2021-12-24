022f307234e9c94b2f4779c53ab69e50535a9bf1
Leandro Lopes <lopesleandro.it@gmail.com>
2021-12-24 03:08:24 -0300

    juste do layout

--- 1-Communication-Between-Components/services-app/src/app/notifications-button/notifications-button.component.ts
+++ 1-Communication-Between-Components/services-app/src/app/notifications-button/notifications-button.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsButtonComponent implements OnInit {
 
+  count: any = 0;
+  
   constructor() { }
 
   ngOnInit(): void {
--- 1-Communication-Between-Components/services-app/src/app/notifications-manager/notifications-manager.component.ts
+++ 1-Communication-Between-Components/services-app/src/app/notifications-manager/notifications-manager.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsManagerComponent implements OnInit {
 
+  count: any = 0;
   constructor() { }
 
   ngOnInit(): void {
