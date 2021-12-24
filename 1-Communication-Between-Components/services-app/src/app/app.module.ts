b0cb71efd420df923d60fa19c6dd59b590a5a575
Leandro Lopes <lopesleandro.it@gmail.com>
2021-12-24 01:55:38 -0300

    replicando os componentes do projeto input-output-app

--- 1-Communication-Between-Components/services-app/src/app/app.module.ts
+++ 1-Communication-Between-Components/services-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NotificationsManagerComponent } from './notifications-manager/notifications-manager.component';
+import { NotificationsButtonComponent } from './notifications-button/notifications-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationsManagerComponent,
+    NotificationsButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ 1-Communication-Between-Components/services-app/src/app/notifications-manager/notifications-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-manager',
+  templateUrl: './notifications-manager.component.html',
+  styleUrls: ['./notifications-manager.component.scss']
+})
+export class NotificationsManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
