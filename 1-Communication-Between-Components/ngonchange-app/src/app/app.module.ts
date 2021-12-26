8b674b1d91506761c961cc8cd167fc198ae0654a
Leandro Lopes <lopesleandro.it@gmail.com>
2021-12-26 20:40:53 -0300

    refatorando o componente de log

--- 1-Communication-Between-Components/ngonchange-app/src/app/app.module.ts
+++ 1-Communication-Between-Components/ngonchange-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VersionControlLogsComponent } from './version-control-logs/version-control-logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VersionControlLogsComponent
   ],
   imports: [
     BrowserModule,
---
+++ 1-Communication-Between-Components/ngonchange-app/src/app/version-control-logs/version-control-logs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-version-control-logs',
+  templateUrl: './version-control-logs.component.html',
+  styleUrls: ['./version-control-logs.component.scss']
+})
+export class VersionControlLogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
