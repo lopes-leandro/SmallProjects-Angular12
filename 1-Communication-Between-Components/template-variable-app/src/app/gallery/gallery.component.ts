83994da5474e58d0ff9574d1e43e3ef36694386b
Leandro Lopes <lopesleandro.it@gmail.com>
2021-12-27 21:46:03 -0300

    criado o componente gallery

--- 1-Communication-Between-Components/template-variable-app/src/app/gallery/gallery.component.ts
+++ 1-Communication-Between-Components/template-variable-app/src/app/gallery/gallery.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  pictures: string[] = [
+    'https://picsum.photos/id/111/400/300',
+    'https://picsum.photos/id/133/400/300',
+    'https://picsum.photos/id/183/400/300',
+    'https://picsum.photos/id/655/400/300',
+    'https://picsum.photos/id/892/400/300',
+    'https://picsum.photos/id/1072/400/300',
+    'https://picsum.photos/id/1071/400/300',
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
