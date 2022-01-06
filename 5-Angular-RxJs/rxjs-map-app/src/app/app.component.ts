51b6bfbabe23beec0492e12d9ea96f384f1ae58f
Leandro Lopes <lopesleandro.it@gmail.com>
2022-01-06 15:29:22 -0300

    aplicando o operador map para transformar o fluxo de dados

--- 5-Angular-RxJs/rxjs-map-app/src/app/app.component.ts
+++ 5-Angular-RxJs/rxjs-map-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,18 @@ export class AppComponent {
 
   subscription: Subscription | null = null;
   inputStreamData = ['bizhub C3110', 'bizhub C3351', 'bizhub C3350', 'bizhub C266', 'bizhub C221'];
-  outputStreamData: number[] = [];
+  outputStreamData: any[] = [];
 
   public startStream(): void {
     const streamSource = interval(1500);
-    this.subscription = streamSource.subscribe(input => {
-      this.outputStreamData.push(input);
-    });
+    this.subscription = streamSource
+      .pipe(
+        map(output => output % this.inputStreamData.length),
+        map(index => this.inputStreamData[index])
+      )
+      .subscribe(element => {
+        this.outputStreamData.push(element);
+      });
   }
 
   public stopStream(): void {
