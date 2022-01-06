e125cb3b60869f1ede51caa643f1b78cb8cf9d91
Leandro Lopes <lopesleandro.it@gmail.com>
2022-01-06 20:54:32 -0300

    distribuindo as funcionalidades entre os novos componentes

--- 5-Angular-RxJs/rxjs-unsubscribe-app/src/app/app.component.ts
+++ 5-Angular-RxJs/rxjs-unsubscribe-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
-import { map, merge } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -9,32 +8,4 @@ import { map, merge } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'Angular 12 com RxJs - Vazamento de Memório com Subscribe';
-
-  subscription: Subscription | null = null;
-  inputStreamData = ['bizhub C3110', 'bizhub C3351', 'bizhub C3350', 'bizhub C266', 'bizhub C221'];
-  inputSerieStreamData = ['Eternals', 'Black Widow', 'Avengers', 'Infinity Gauntlet', 'Star Wars', 'The Amazing Spider-Man', 'Moon Knight', 'Vision'];
-  outputStreamData: any[] = [];
-
-  public startStream(): void {
-    const streamSource = interval(1500);
-    const seriesSource = interval(1000)
-      .pipe(
-        map(output => output % this.inputSerieStreamData.length),
-        map(index => this.inputSerieStreamData[index])
-      )
-    this.subscription = streamSource
-      .pipe(
-        map(output => output % this.inputStreamData.length),
-        map(index => this.inputStreamData[index]),
-        merge(seriesSource)
-      )
-      .subscribe(element => {
-        this.outputStreamData.push(element);
-      });
-  }
-
-  public stopStream(): void {
-    this.subscription?.unsubscribe();
-    this.subscription = null;
-  }
 }
