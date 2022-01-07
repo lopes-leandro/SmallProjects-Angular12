15e4d27d9bdc0f639504086712d7366baf4677cc
Leandro Lopes <lopesleandro.it@gmail.com>
2022-01-07 11:40:54 -0300

    criando os componentes base para o inicio do projeto

---
+++ 5-Angular-RxJs/rxjs-flatmap-operator-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { UserCardComponent } from './shared/components/user-card/user-card.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    LoaderComponent,
+    UserCardComponent,
+    UserDetailComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
